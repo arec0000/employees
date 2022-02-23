84e953d870d1ee86824ba615128d7ba742bc2639
Nikita <s2015836@x.donstu.ru>
2022-02-23 11:46:28 +0300

    92. add props to employees list

--- src/components/employees-list/employees-list.js
+++ src/components/employees-list/employees-list.js
@@ -2,12 +2,17 @@ import EmployeesListItem from '../employees-list-item/employees-list-item';
 
 import './employees-list.css';
 
-const Employeeslist = () => {
+const Employeeslist = ({data}) => {
+
+    const elements = data.map(item => {
+        return (
+            <EmployeesListItem {...item}/>
+        );
+    });
+
     return (
         <ul className="app-list list-group">
-            <EmployeesListItem/>
-            <EmployeesListItem/>
-            <EmployeesListItem/>
+            {elements}
         </ul>
     );
 };
